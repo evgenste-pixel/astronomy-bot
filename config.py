a080548f7e1fecfb08991c63d8807c06a4cc467b
evgenste-pixel <evgenste@yandex.ru>
2026-06-22 23:30:42 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Токен бота (получить у @BotFather)
-BOT_TOKEN = "8802916174:AAGSXntblq3n7Od7Hhfr4-of0iLkysyIAJ4"
-
-# Ссылка на ваше мини-приложение (будет после загрузки на GitHub Pages)
-# Пример: https://username.github.io/astronomy-bot/
-WEBAPP_URL = "https://ВАШ_АККАУНТ.github.io/astronomy-bot/"
-
